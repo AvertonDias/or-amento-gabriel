410f0f9063a9c6412d685296cd1317442014a15d
Averton Silva Dias <verton3@gmail.com>
2025-10-24 23:48:37 +0000

    se estava funcionando antes pq não está funcionando agora?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDiKZq6bOkazeGAbh-bpjePrOeT5EhPX_0",
+  authDomain: "gutters-budget-pro.firebaseapp.com",
+  projectId: "gutters-budget-pro",
+  storageBucket: "gutters-budget-pro.appspot.com",
+  messagingSenderId: "766057124102",
+  appId: "1:766057124102:web:a8b2ed8d064964e4980e87"
 };
 
 // Initialize Firebase
