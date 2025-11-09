faa79d75f9119fba75114c3aa4584760dd1b51c3
Averton Silva Dias <verton3@gmail.com>
2025-11-09 16:31:17 +0000

    Try fixing this error: `Server Error: Error: Cannot find module './682.j

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,14 @@
+
 /** @type {import('next').NextConfig} */
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
 });
 
 const nextConfig = {
-  // sua configuração do Next.js aqui
+  reactStrictMode: true,
 };
 
 export default withPWA(nextConfig);
