e27710b102ce69dd9eab85b03698c266bc58e795
Averton Silva Dias <verton3@gmail.com>
2025-11-16 15:36:07 +0000

    veja novamente

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig = {
-  // sua configuração normal do Next.js aqui
+  reactStrictMode: true,
+  // Outras configurações do Next.js podem vir aqui
 };
 
 const pwaConfig = withPWA({
@@ -11,22 +12,7 @@ const pwaConfig = withPWA({
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
-    // Estratégias de cache
-    {
-      urlPattern: /^https$?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'https-calls',
-        networkTimeoutSeconds: 15,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
+    // Estratégias de cache podem ser adicionadas aqui
   ],
 });
 
