41e039e93bac010c3034b5a09331883d94ed927a
Averton Silva Dias <verton3@gmail.com>
2025-11-16 15:34:48 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,33 @@
 /** @type {import('next').NextConfig} */
 import withPWA from '@ducanh2912/next-pwa';
 
-const pwa = withPWA({
+const nextConfig = {
+  // sua configuração normal do Next.js aqui
+};
+
+const pwaConfig = withPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  cacheStartUrl: true,
-  dynamicOptions: {
-    // Exemplo de cache para fontes do Google
-    'https://fonts.googleapis.com/.*': {
-      cacheName: 'google-fonts-cache',
-      strategy: 'CacheFirst',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    // Estratégias de cache
+    {
+      urlPattern: /^https$?.*/,
+      handler: 'NetworkFirst',
       options: {
+        cacheName: 'https-calls',
+        networkTimeoutSeconds: 15,
+        expiration: {
+          maxEntries: 150,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
         cacheableResponse: {
           statuses: [0, 200],
         },
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Dias
-        },
       },
     },
-    'https://fonts.gstatic.com/.*': {
-        cacheName: 'google-fonts-cache',
-        strategy: 'CacheFirst',
-        options: {
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Dias
-          },
-        },
-    }
-  },
+  ],
 });
 
-const nextConfig = {
-  // Sua configuração Next.js aqui
-};
-
-export default pwa(nextConfig);
+export default pwaConfig(nextConfig);
