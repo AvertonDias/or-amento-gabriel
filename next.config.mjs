108196adaa9c49a05f616a8019a99811a38fcf21
Averton Silva Dias <verton3@gmail.com>
2025-11-16 15:33:22 +0000

    da uma reforçada na funçao PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,47 @@
-import withPWA from "@ducanh2912/next-pwa";
+/** @type {import('next').NextConfig} */
+import withPWA from '@ducanh2912/next-pwa';
 
-const pwaConfig = withPWA({
-  dest: "public",
+const pwa = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  cacheStartUrl: true,
+  dynamicOptions: {
+    // Exemplo de cache para fontes do Google
+    'https://fonts.googleapis.com/.*': {
+      cacheName: 'google-fonts-cache',
+      strategy: 'CacheFirst',
+      options: {
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Dias
+        },
+      },
+    },
+    'https://fonts.gstatic.com/.*': {
+        cacheName: 'google-fonts-cache',
+        strategy: 'CacheFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Dias
+          },
+        },
+    }
+  },
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your next config here
+  // Sua configuração Next.js aqui
 };
 
-export default pwaConfig(nextConfig);
+export default pwa(nextConfig);
