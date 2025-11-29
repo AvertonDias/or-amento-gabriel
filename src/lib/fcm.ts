1903d509ec73c8dc08da0dd2a01982fa37fdccf0
Averton Silva Dias <verton3@gmail.com>
2025-11-29 01:06:37 +0000

    já estava ativa

--- src/lib/fcm.ts
+++ src/lib/fcm.ts
@@ -4,31 +4,50 @@
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { app } from './firebase';
 
-export const requestForToken = () => {
-  const messaging = getMessaging(app);
-  if (!messaging) return null;
+// Variável para armazenar a instância do Messaging
+let messagingInstance: ReturnType<typeof getMessaging> | null = null;
 
+// Função para inicializar o Messaging de forma segura no cliente
+const getMessagingInstance = () => {
+  if (typeof window !== 'undefined' && !messagingInstance) {
+    messagingInstance = getMessaging(app);
+  }
+  return messagingInstance;
+};
+
+
+export const requestForToken = () => {
+  const messaging = getMessagingInstance();
+  if (!messaging) {
+    console.log("Messaging service not available.");
+    return null;
+  }
+
+  console.log("Requesting FCM token...");
   return getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_FCM_VAPID_KEY })
     .then((currentToken) => {
       if (currentToken) {
-        console.log('FCM Token:', currentToken);
-        // You can send this token to your server to send notifications to this device
+        console.log('FCM Token obtido:', currentToken);
+        // Você pode enviar este token para seu servidor para enviar notificações para este dispositivo
       } else {
-        console.log('No registration token available. Request permission to generate one.');
+        console.log('Nenhum token de registro disponível. Solicite permissão para gerar um.');
       }
     })
     .catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
+      console.error('Ocorreu um erro ao recuperar o token. ', err);
     });
 };
 
 export const onMessageListener = () => {
-    const messaging = getMessaging(app);
-    if (!messaging) return null;
+    const messaging = getMessagingInstance();
+    if (!messaging) {
+        console.log("Messaging service not available for listener.");
+        return Promise.reject("Messaging not supported");
+    }
 
     return new Promise((resolve) => {
         onMessage(messaging, (payload) => {
-            console.log('Message received. ', payload);
+            console.log('Mensagem recebida. ', payload);
             resolve(payload);
         });
     });
