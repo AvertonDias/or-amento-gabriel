a47ef7d7c2b984c9e338e4bd2f8304f6100c1f9d
Averton Silva Dias <verton3@gmail.com>
2025-12-04 01:09:20 +0000

    studio-6755913747:~/studio{main}$ npm run build
    > nextn@0.1.0 build
    > n

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,19 @@
-/** @type {import('next').NextConfig} */
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // Exclui a rota da API do cache para garantir que as chamadas sempre cheguem ao servidor
-  exclude: [
-    ({ asset, compilation }) => {
-      if (
-        asset.name.startsWith("server/") ||
-        asset.name.match(/^((app-pages-manifest|build-manifest|react-loadable-manifest|middleware-manifest|middleware-build-manifest|subresource-integrity-manifest|build-manifest)\.json|favicon\.ico|manifest\.(webmanifest|json)|_next\/static\/|sw\.js|sw-esm\.js)$/)
-      ) {
-        return true;
-      }
-      return false;
-    },
-  ],
+import withPWA from "@ducanh2912/next-pwa";
+
+const pwaConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  fallbacks: {
+    document: "/_offline", // Rota para a página offline
+  },
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // sua configuração do Next.js pode ir aqui
+  // Suas outras configurações do Next.js podem vir aqui
 };
 
-module.exports = withPWA(nextConfig);
+export default pwaConfig(nextConfig);
