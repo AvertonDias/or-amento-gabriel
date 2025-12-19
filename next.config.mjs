84250feb76360bc8b293b7907c640056a2b5b140
Averton Silva Dias <verton3@gmail.com>
2025-12-19 19:45:05 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,56 +1,19 @@
 // @ts-check
 
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPwaInit from '@ducanh2912/next-pwa';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Outras configurações do Next.js podem vir aqui
+  // Configurações do Next.js
 };
 
-const withPWA = withPWAInit({
+const withPwa = withPwaInit({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: isDev, // Desativa o PWA em ambiente de desenvolvimento
   register: true,
   skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: ({ request }) => request.mode === 'navigate',
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'pages',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|ico|webp)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js|css)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-resources',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-  ],
-  fallbacks: {
-    document: '/_offline', // Rota para a página offline
-  },
 });
 
-export default withPWA(nextConfig);
+export default withPwa(nextConfig);
