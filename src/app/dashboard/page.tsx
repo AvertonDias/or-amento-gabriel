9a1e669a58414be67909fde9a0278d186c5188e7
Averton Silva Dias <verton3@gmail.com>
2026-01-06 15:51:15 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Element type is

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,6 @@
-'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/orcamento');
-  }, [router]);
-
-  return null;
+export default function DashboardPage() {
+  redirect('/dashboard/orcamento');
 }
